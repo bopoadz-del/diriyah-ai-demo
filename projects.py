bc1b52dbdbcd37461dcbc5c274058d00a5eb40d0
bopoadz-del <bopoadz@gmail.com>
2025-09-08 16:48:33 +0300

    Update projects.py

--- projects.py
+++ projects.py
@@ -1,4 +1,13 @@
-async def fetch_projects():
-    return [
-        {"id":1,"name":"Opera House – Foundation","location":"Diriyah","description":"Structural works","status":"On Track","progress":68,"deadline":"Dec 1, 2025","cloudService":"google","riskLevel":"medium"}
-    ]
+PROJECTS = [
+    {
+        "id": 1,
+        "name": "Opera House – Foundation",
+        "location": "Diriyah",
+        "description": "Structural works",
+        "status": "On Track",
+        "progress": 68,
+        "deadline": "Dec 1, 2025",
+        "cloudService": "google",
+        "riskLevel": "medium"
+    }
+]

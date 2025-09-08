12c6e898e8f0af79b9ebaf8a33f41ba0595b18c8
bopoadz-del <bopoadz@gmail.com>
2025-09-08 15:18:29 +0300

    Delete projects.py

--- projects.py
+++
@@ -1,4 +0,0 @@
-async def fetch_projects():
-    return [
-        {"id":1,"name":"Opera House – Foundation","location":"Diriyah","description":"Structural works","status":"On Track","progress":68,"deadline":"Dec 1, 2025","cloudService":"google","riskLevel":"medium"}
-    ]
